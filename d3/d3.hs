45b3584548fb58705ffa30c883abaf95732dc371
Eric Pedley <ericpedley@gmail.com>
2022-12-08 11:27:16 -0800

    day 3 part 2

--- d3/d3.hs
+++ d3/d3.hs
@@ -4,12 +4,9 @@ import Data.Char
 -- ord 'a' = 97
 -- ord 'A' = 65
 splitLines s = splitOn "\n" s
-myIntersect :: ([Char], [Char]) -> Set Char
-myIntersect t = intersection (fromList $ fst t) (fromList $ snd t)
 solve s = sum [ if char>=97 then char-96 else 27+char-65 
-              | line<-splitLines s
-              , let charSets = splitAt (length line `div` 2) line
-              , let char = ord $ findMin $ myIntersect charSets ]
+              | group <- map (map fromList) $ chunksOf 3 $ splitLines s
+              , let char = ord $ findMin $ intersection (group !! 0) (intersection (group !! 1) (group !! 2)) ]
 main = do
     content <- getContents
     print $ solve content
