ce611613a86cbb05173c17629489e3697600782a
Eric Pedley <ericpedley@gmail.com>
2022-12-11 12:47:59 -0800

    complete day 6

--- d6/d6.hs
+++ d6/d6.hs
@@ -1,8 +1,8 @@
 import qualified Data.Set as Set
 
 solve s i = let
-    uniqueChars = Set.size $ Set.fromList $ take 4 s
-    in if uniqueChars==4 then i+4 else solve (drop 1 s) i+1
+    uniqueChars = Set.size $ Set.fromList $ take 14 s
+    in if uniqueChars==14 then i+14 else solve (drop 1 s) i+1
 main = do
     content <- getContents
     print $ solve content 0
