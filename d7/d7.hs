d289438d2aaf7905b94f6e1b0ce6b4d52517d19a
Eric Pedley <ericpedley@gmail.com>
2022-12-11 19:53:54 -0800

    finished day 7

--- d7/d7.hs
+++ d7/d7.hs
@@ -1,5 +1,6 @@
 import Data.List.Split
 import qualified Data.Map as HM
+import qualified Data.Set as Set
 
 insertVals :: HM.Map String Int -> [String] -> Int -> HM.Map String Int
 insertVals m keys value =
@@ -9,42 +10,52 @@ insertVals m keys value =
         newMap = HM.insertWith (+) (head keys) value m   
         in insertVals newMap (tail keys) value
 
-buildMap :: HM.Map String Int -> [String] -> [String] -> HM.Map String Int
-buildMap m lines directoryStack = if (length lines) > 0
+buildMap :: HM.Map String Int -> [String] -> [String] -> HM.Map String Int -> HM.Map String Int
+buildMap m lines directoryStack visitedSet = if (length lines) > 0
     then let
         line = lines !! 0
         in if (line !! 0) == '$'
             then let
                 command = take 2 (drop 2 line)
+                directory = drop 5 line
+                fullPath = (head directoryStack) ++ directory ++ "/"
                 in if command == "cd"
-                    then let
-                        directory = drop 5 line
-                        in if directory == "/"
-                            then
-                                buildMap m (drop 1 lines) [""]
-                            else if directory == ".."
-                                then buildMap m (drop 1 lines) (tail directoryStack)
-                            else
-                                buildMap m (drop 1 lines) ([(foldl1 (++) directoryStack) ++ "/" ++ directory] ++ directoryStack)
+                    then if directory == "/"
+                        then
+                            buildMap m (drop 1 lines) ["/"] visitedSet
+                        else if directory == ".."
+                            then buildMap m (drop 1 lines) (tail directoryStack) visitedSet
+                        else
+                            buildMap m (drop 1 lines) ([fullPath] ++ directoryStack) visitedSet
                     else
-                        buildMap m (drop 1 lines) directoryStack
+                        buildMap m (drop 1 lines) directoryStack (HM.insertWith (+) (head directoryStack) 1 visitedSet)
             else let
                 tokens = splitOn " " line
                 in if (tokens !! 0) == "dir"
                     then
-                        buildMap m (drop 1 lines) directoryStack
+                        buildMap m (drop 1 lines) directoryStack visitedSet
                     else let
-                        newMap = insertVals m directoryStack (read (tokens !! 0) :: Int) 
-                        in buildMap newMap (drop 1 lines) directoryStack
+                        newMap = if (HM.findWithDefault 2 (head directoryStack) visitedSet) > 1
+                            then m
+                            else
+                                insertVals m directoryStack (read (tokens !! 0) :: Int) 
+                        in buildMap newMap (drop 1 lines) directoryStack visitedSet
                 
     else m
 
 solve s = let
     lines = splitOn "\n" s
-    in sum $ filter ((>=) 100000) $ snd $ unzip $ HM.toList $ buildMap HM.empty lines ["/"]
+    -- in HM.toList $ buildMap HM.empty lines ["/"] HM.empty
+    values = snd $ unzip $ HM.toList $ buildMap HM.empty lines ["/"] HM.empty
+    biggest = maximum values
+    in minimum $ filter ((<=) (30000000- (70000000 - biggest))) values 
 main = do
     content <- getContents
     print $ solve content
 -- 210136316 too high
 
 -- 1038055 too low
+
+-- pt2:
+-- 44795677 too high
+-- 41523658 too high
